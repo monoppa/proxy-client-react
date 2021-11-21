b768b5e5a49cd84d16a52432b30b7de46a22960d
mon <raquindoza14@gmail.com>
2021-11-21 23:06:11 +0800

    remove debuggers

--- src/useFlag.ts
+++ src/useFlag.ts
@@ -4,16 +4,13 @@ import FlagContext from "./FlagContext";
 const useFlag = (name: string) => {
   const { isEnabled, client } = useContext(FlagContext);
   const [flag, setFlag] = useState(!!isEnabled(name));
-  console.log("file: useFlag.ts - line 7 - flag", flag);
   const flagRef = useRef<any>();
   flagRef.current = flag;
 
   useEffect(() => {
-    console.log("here");
     if (!client) return;
     client.on("update", () => {
       const enabled = isEnabled(name);
-      console.log("update", enabled);
       if (enabled !== flagRef.current) {
         flagRef.current = enabled;
         setFlag(!!enabled);
@@ -22,7 +19,6 @@ const useFlag = (name: string) => {
 
     client.on("ready", () => {
       const enabled = isEnabled(name);
-      console.log("ready", enabled);
       setFlag(enabled);
     });
   }, [client]);
