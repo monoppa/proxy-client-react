1a1fa0de35a259cdb465d12eb0bc9f677eee9308
mon <raquindoza14@gmail.com>
2021-11-21 22:09:52 +0800

    debug client ready

--- src/useFlag.ts
+++ src/useFlag.ts
@@ -4,10 +4,12 @@ import FlagContext from "./FlagContext";
 const useFlag = (name: string) => {
   const { isEnabled, client } = useContext(FlagContext);
   const [flag, setFlag] = useState(!!isEnabled(name));
+  console.log("file: useFlag.ts - line 7 - flag", flag);
   const flagRef = useRef<any>();
   flagRef.current = flag;
 
   useEffect(() => {
+    console.log("here");
     if (!client) return;
     client.on("update", () => {
       const enabled = isEnabled(name);
